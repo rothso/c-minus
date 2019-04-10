b3dc968b0610bb4f15298635982e0601c1c16064
Rothanak So <rothanak.hello@gmail.com>
2019-04-10 12:07:41 -0400

    Fix output format

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
 import sys
 from lexer import lex
 from parser import parse
 from semantics import analyze
 from codegen import to_ir
 
+
+def display(num, line):
+    instruction, source1, source2, dest = line
+    print(f'{num + 1:<4}{instruction:10}{source1 or "":10}{source2 or "":10}{dest or ""}')
+
+
 with open(sys.argv[1], 'r') as f:
-    [print(line) for line in to_ir(analyze(parse(lex(f.read()))))]
+    [display(i, line) for i, line in enumerate(to_ir(analyze(parse(lex(f.read())))))]
